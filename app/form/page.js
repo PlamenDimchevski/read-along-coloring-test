ba925e4f962e67dece7b8476bb627e1bf61ee651
PlamenDimchevski <pdimchevske@gmail.com>
2024-01-13 20:51:14 +0200

    template playground

--- app/form/page.js
+++ app/form/page.js
@@ -2,9 +2,8 @@ import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 
 export default function Example() {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
     <form>
-    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
@@ -219,7 +218,7 @@ export default function Example() {
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            We&apos;ll always let you know about important changes, but you pick what else you want to hear about.
+            We'll always let you know about important changes, but you pick what else you want to hear about.
           </p>
 
           <div className="mt-10 space-y-10">
@@ -331,15 +330,5 @@ export default function Example() {
         </button>
       </div>
     </form>
-     <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
-     <div
-       className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-       style={{
-         clipPath:
-           'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-       }}
-     />
-   </div>
-   </div>
   )
 }
