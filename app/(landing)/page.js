943fc373bf5b3cd678a096f55f10247800c5ef34
PlamenDimchevski <pdimchevske@gmail.com>
2024-01-19 14:34:49 +0200

    text chage

--- app/(landing)/page.js
+++ app/(landing)/page.js
@@ -25,7 +25,7 @@ export default function Home() {
           </svg>
           <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
             <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              This is based tool for read-alongs.
+              This tool is for group read-alongs.
             </h2>
             <p className="mt-6 text-lg leading-8 text-gray-300">
               In order to use it, you need to be part of the book club on
@@ -36,7 +36,7 @@ export default function Home() {
                 href={config.generatedURL}
                 className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
-                Get started{" "}
+                Get started
               </a>
               <a
                 href="#"
